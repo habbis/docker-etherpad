2537ff23dcc21fc04fbc3b9ad3e01b3abceffc71
habbis <habbis@medmail.ch>
2020-05-22 21:13:09 +0200

    adding dockerfile

--- requirements.sh
+++ requirements.sh
@@ -3,17 +3,25 @@
 
 get-etherpad () {
 if test -d etherpad-lite
-	then echo "exists"
+	then echo "etherpad-lite exists"
 	else git clone --branch master git://github.com/ether/etherpad-lite.git
 fi
 }
 
 get-nodejs () {
 if test -s node-v12.16.3-linux-x64.tar.xz
-	then echo "exists"
+	then echo "nodejs exists"
 	else wget https://nodejs.org/dist/v12.16.3/node-v12.16.3-linux-x64.tar.xz
+fi
+}
+
+get-settings () {
+if test -e settings.json
+	then echo "settings.json exists"
+	else wget https://raw.githubusercontent.com/habbis/etherpad-settings/master/settings.json
 fi
 }
 
 get-etherpad
 get-nodejs
+get-settings
